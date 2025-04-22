9512efbcd9810bbb7e20579068dcc0b9394f7695
Ronald Spinelli <70922677+SpinelliRonald17@users.noreply.github.com>
2025-04-23 00:13:43 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,7 +18,41 @@
         $(window).resize(toggleNavbarMethod);
     });
     
+     /* --pocicion inicial */
+let ubicacionPrincipal = window.pageYOffset;
+let $nav = document.querySelector("nav");
+
+    /* --evento scroll */
+    window.addEventListener("scroll", function() {
+    /* --muestra la ubicacion cada vez que hagas scroll */
+    //console.log(window.pageYOffset);
+
+    /* --donde nos encontramos actualmente */
+    let desplazamientoActual = window.pageYOffset;
+
+    /* --condicon para ocultar o mostrar el menu */
+    if(ubicacionPrincipal >= desplazamientoActual) {
+        /* --si es mayor o igual se muesta */
+        $nav.style.top = "0px";
+        console.log('Ubicacion Principal')
+        console.log(ubicacionPrincipal)
+        console.log('desplazamiento')
+        console.log(desplazamientoActual)
+    } else {
+        /* --sino lo ocultamos añadiendo un top negativo */
+        $nav.style.top = "-80px";
+        console.log('Ubicacion Principal')
+        console.log(ubicacionPrincipal)
+        console.log('desplazamiento')
+        console.log(desplazamientoActual)
+    }
+
+    /* --actulizamos la ubicacion principal */
+    ubicacionPrincipal = desplazamientoActual;
+});
     
+
+   
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
